544e1087f7c12ed6d6e9eccdbe20b0c71b3088ed
ash-stew <43679070+ash-stew@users.noreply.github.com>
2022-08-17 12:18:50 +0100

    Unittests

---
+++ HttpGetRequestTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Crudops_Console_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crudops_Console_Application.Tests
+{
+    [TestClass()]
+    public class HttpGetRequestTests
+    {
+        [TestMethod()]  // // This checks that the URL is valid with a successful internet connection.
+        public async Task CheckSuccessfulResponceTest() 
+        {
+            // arrange
+            HttpGetRequest request = new HttpGetRequest();
+            int retryCounter = 0;
+            bool success = false;
+
+            // act
+            await request.GetRequest("https://herrcomockapifunc.azurewebsites.net//api//getProducts");
+
+            while(!request.response.IsSuccessStatusCode && retryCounter < 10 )
+            {
+                Thread.Sleep(4000);
+                await request.GetRequest("https://herrcomockapifunc.azurewebsites.net//api//getProducts");
+                retryCounter++;
+            }
+
+            if(request.Message.Equals("") && request.response.IsSuccessStatusCode)
+            {
+                success = true;
+            }
+
+            // assert
+            Assert.AreEqual(success, true);
+        }
+
+        
+        
+    }
+}
---
+++ SheetGeneratorTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Crudops_Console_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace Crudops_Console_Application.Tests
+{
+    [TestClass()]
+    public class SheetGeneratorTests
+    {
+        
+
+        [TestMethod()]
+        public void checkValidJSONTest()
+        {
+            // Arrange
+            HttpGetRequest request = new HttpGetRequest();
+            request.GetRequest("https://herrcomockapifunc.azurewebsites.net//api//getProducts");
+            int retryCounter = 0;
+
+            // retry, if necessary
+            while(request.Content.Equals("") && retryCounter < 10)
+            {
+                Thread.Sleep(4000);
+                request.GetRequest("https://herrcomockapifunc.azurewebsites.net//api//getProducts");
+                retryCounter++;
+            }
+          
+            JsonSchema schema = JsonSchema.Parse(request.Content);
+            JObject jObject = JObject.Parse(@"{
+            'id': 2,
+            'name': 'aSheet' 
+             }");
+
+            // Act
+            bool valid = jObject.IsValid(schema);
+
+            // Assert
+            Assert.AreEqual(valid, true);
+        }
+    }
+}
